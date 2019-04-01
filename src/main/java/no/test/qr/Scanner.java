3a8991319247f233f9790c41bd9ecbc7383e22ea
Guillaume Facchini <guillaume.facchini@zenika.com>
2019-04-01 19:23:10 +0200

    Add property file + insert into sql

--- src/main/java/no/test/qr/Scanner.java
+++ src/main/java/no/test/qr/Scanner.java
@@ -9,43 +9,102 @@ import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Properties;
 
 
 public class Scanner {
 
     private Logger logger = LoggerFactory.getLogger(Scanner.class);
-    private static final String FILE_NAME = "/run/shm/scan.png";
+    private static final String FILE_NAME = "/tmp/scan.png";
+    private String DB_ADDRESS;
+    private String DB_PASSWORD;
+    private String DB_TABLE;
+    private String READER_NUMBER;
+    private Properties properties;
+
+
+    private final QRCodeReader reader;
+
+
+    public Scanner() {
+        this.reader = new QRCodeReader();
+        this.properties = new Properties();
+        BufferedInputStream stream = null;
+        try {
+            stream = new BufferedInputStream(new FileInputStream("qr-scan.properties"));
+            properties.load(stream);
+            stream.close();
+            this.DB_ADDRESS = properties.getProperty("db_address");
+            this.DB_PASSWORD = properties.getProperty("db_password");
+            this.DB_TABLE = properties.getProperty("db_table");
+            this.READER_NUMBER = properties.getProperty("reader_number");
+
+        } catch (FileNotFoundException e) {
+            logger.error("Properties file not found 'qr-scan.properties'", e);
+        } catch (IOException e) {
+            logger.error("Could not load properties", e);
+        }
+
+
+    }
 
     public static void main(String[] args) {
-        while(true) {
+        while (true) {
             new Scanner().scan();
         }
     }
 
     public String scan() {
         String result = null;
+        Statement stmt;
+        Connection c = null;
         try {
-            Result r = new QRCodeReader().decode(acquireBitmapFromCamera());
-            result = r.getText();
+            result = reader
+                    .decode(acquireBitmapFromCamera())
+                    .getText();
             logger.info("Scan Decode is successful: " + result);
 
             System.out.println(result);
 
+
+            Calendar calendar = Calendar.getInstance();
+            int hours = calendar.get(Calendar.HOUR_OF_DAY);
+            int minutes = (calendar.get(Calendar.MINUTE) / 5) * 5;
+
+            Class.forName("org.postgresql.Driver");
+            DriverManager.getConnection("jdbc:postgresql://" + DB_ADDRESS + ":5432/postgres",
+                    "postgres", DB_PASSWORD);
+
+
+            stmt = c.createStatement();
+            String sql = "INSERT INTO " + DB_TABLE + " (qr, c" + READER_NUMBER + ")" +
+                    "VALUES (" + result + "," + hours + ":" + minutes + ")";
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+
         } catch (NotFoundException e) {
-            logger.error("QR Code was not found in the image. It might have been partially detected but could not be confirmed.");
+            //logger.error("QR Code was not found in the image. It might have been partially detected but could not be confirmed.");
         } catch (ChecksumException e) {
             logger.error("QR Code was successfully detected and decoded, but was not returned because its checksum feature failed.");
         } catch (FormatException e) {
             logger.error("QR Code was successfully detected, but some aspect of the content did not conform to the barcode's format rules. This could have been due to a mis-detection.");
         } catch (InterruptedException e) {
-           logger.error("Error acquiring bitmap", e);
+            logger.error("Error acquiring bitmap", e);
         } catch (IOException e) {
             logger.error("I/O error acquiring bitmap: {}", e.getMessage());
+        } catch (SQLException e) {
+            logger.error("I/O error connection to database", e.getMessage());
         } catch (Exception e) {
-            logger.error("error acquiring bitmap", e);
+            logger.error("Unknown Error", e);
         }
 
         return result;
@@ -72,4 +131,6 @@ public class Scanner {
         int code = process.waitFor();
         logger.trace("Exit code: " + code);
     }
+
+
 }
